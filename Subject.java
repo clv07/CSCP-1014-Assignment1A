7c458fdeb211ca81eed0349878a3e96798465ed6
Livia Chandra <76584280+clv07@users.noreply.github.com>
2022-11-28 14:54:17 -0600

    Add files via upload
    Adding object class files

---
+++ Subject.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Subject {
+
+	//subject ID
+	public final static String [] SUB_ID = {"ACCT2013", "CHEM1033",
+											"CHEM1053", "CSCP1014", 
+											"CSCP2014", "ENGL1014",
+											"JRNS1013",	"MATH1034", 
+											"MATH1044", "MATH2014"};
+	
+	//subject name
+	public final static String [] SUB_NAME = {"Basic Principle in Accounting", 
+											"General Chemistry 1",
+											"General Chemistry 2",
+											"Programming 1",
+											"Programming 2",
+											"Fundamentals of College Writing",
+											"Introduction to Journalism",
+											"Calculus 1",
+											"Calculus 2",
+											"Calculus 3"};
+	
+}
